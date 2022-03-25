0639cbeedc0724339fec060d5b13a1e32c81d7c1
shahnmah <shahn.mah@gmail.com>
2022-03-26 04:19:24 +0600

    cart and json added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,18 +2,18 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
-
+    // cart.map(cartName => console.log(cartName.name))
     // console.log(cart)
     // console.log(cart[0].name)
     let productName = [];
     for(const product of cart){
         productName = product.name
-        
     }
     return (
         <div className='ms-3 px-4 cart text-white'>
+            <h4 className='py-3'>Selected Items</h4>
             <p>{cart.length}</p>
-            <h4>{productName}</h4>
+            <h5>{productName}</h5>
             <button className='btn btn-info text-white fw-bold my-2'>Choose One</button>
             <button className='btn btn-danger fw-bold'>Choose Again</button>
             <div id='a'></div>
